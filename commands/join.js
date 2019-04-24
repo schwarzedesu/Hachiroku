e68544e9ba41110f7056300cb722d6ce663b8f9f
Schwarze <schwarzedesu@gmail.com>
2019-04-24 15:19:49 -0500

    Updating stuff

--- commands/join.js
+++ commands/join.js
@@ -1,18 +1,22 @@
+const fs = require('fs');
+const ytdl = require('ytdl-core');
+
 module.exports = message => {
 
   if (message.member.voiceChannel) {
     message.member.voiceChannel.join()
       .then(connection => {
-        message.reply('I have successfully connected to the channel!');
-        //const dispatcher = connection.playFile('Kokoro_Odoru_ori.mp3');
+        message.channel.send(`I have successfully connected to the channel, ${message.author}-sama!`);
 
-        dispatcher.on('error', e => {
-          console.log(e);
-        });
+        ytdl('http://www.youtube.com/watch?v=A02s8omM_hI')
+          .pipe(fs.createWriteStream('video.flv'));
+
+        connection.playArbitraryInput('https://azurlane.koumakan.jp/w/images/0/03/Unicorn_SelfIntroJP.ogg');
+
       })
       .catch(console.log);
   } else {
-    message.reply(`You need to join a voice channel first, ${message.author.username}-sama~`);
-    //message.reply(`You need to join a voice channel first~`);
+    message.channel.send(`You need to join a voice channel first, ${message.author.username}-sama~`);
+
   }
 }
