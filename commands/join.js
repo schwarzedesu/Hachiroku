39c9ebbce1a384cda5c71372dab17be19a67f2f3
Schwarze <schwarzedesu@gmail.com>
2019-05-01 23:50:56 -0500

    Added conversions for video/audio file downloads

--- commands/join.js
+++ commands/join.js
@@ -1,18 +1,35 @@
 const fs = require('fs');
 const ytdl = require('ytdl-core');
+var ffmpeg = require('ffmpeg');
 
 module.exports = message => {
 
   if (message.member.voiceChannel) {
     message.member.voiceChannel.join()
       .then(connection => {
-        message.channel.send(`I have successfully connected to the channel, ${message.author}-sama!`);
+        message.channel.send(`Connected, ${message.author}-sama!`);
 
         ytdl('http://www.youtube.com/watch?v=A02s8omM_hI')
           .pipe(fs.createWriteStream('video.flv'));
 
-        connection.playArbitraryInput('https://azurlane.koumakan.jp/w/images/0/03/Unicorn_SelfIntroJP.ogg');
+        try {
+        	var process = new ffmpeg('video.flv');
+        	process.then(function (video) {
+            //Callback mode
+            video.fnExtractSoundToMP3('video.flv', function (error, file) {
+              if(!error){
+                console.log('Audio file:' + file);
+              }
+              console.log(error);
+            });
+        	}, function (err) {
+        		console.log('Error: ' + err);
+        	});
+        } catch (e) {
+        	console.log(e);
+        }
 
+        connection.playArbitraryInput('https://azurlane.koumakan.jp/w/images/0/03/Unicorn_SelfIntroJP.ogg');
       })
       .catch(console.log);
   } else {
